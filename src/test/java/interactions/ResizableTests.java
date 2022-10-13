46cb772b429fb94559a92941f6ec43f1c3f7d64c
JonatanPilichowski <108926973+JonatanPilichowski@users.noreply.github.com>
2022-10-13 21:28:42 +0200

    added sortable, fixed resizable

--- src/test/java/interactions/ResizableTests.java
+++ src/test/java/interactions/ResizableTests.java
@@ -11,15 +11,40 @@ import org.openqa.selenium.interactions.Actions;
 import setup.TestBase;
 
 public class ResizableTests extends TestBase {
+    private final int decreasedValueOfClick = 18;
+
     @Test
     @DisplayName("Resize widow few times")
     @Tag("resize")
     public void resizeWindow() {
+
+        final int resizeByPixels = 10;
         driver.get("http://51.75.61.161:9102/resizable.php");
+        WebElement resizableField = driver.findElement(By.id("resizable"));
+        Dimension resFieldSizeOriginal = resizableField.getSize();
         WebElement resizeBtn = driver.findElement(By.cssSelector("div[class*='ui-icon-gripsmall-diagonal-se']"));
+        SoftAssertions softly = new SoftAssertions();
         Actions actions = new Actions(driver);
-        actions.dragAndDropBy(resizeBtn, 10, 0).perform();
-        actions.dragAndDropBy(resizeBtn, 0, 10).perform();
-        actions.dragAndDropBy(resizeBtn, 10, 10).perform();
+        actions.dragAndDropBy(resizeBtn, resizeByPixels+decreasedValueOfClick, 0).perform();
+        softly.assertThat(getResFieldSize().getWidth()).isEqualTo(getExpectedWidth(resizeByPixels, resFieldSizeOriginal));
+        actions.dragAndDropBy(resizeBtn, 0, resizeByPixels+decreasedValueOfClick).perform();
+        softly.assertThat(getResFieldSize().getHeight()).isEqualTo(getExpectedHeight(resizeByPixels, resFieldSizeOriginal));
+        Dimension  resFieldSizeCurr = resizableField.getSize();
+        actions.dragAndDropBy(resizeBtn, resizeByPixels+decreasedValueOfClick, resizeByPixels+decreasedValueOfClick).perform();
+        softly.assertThat(getResFieldSize()).isEqualTo(new Dimension(getExpectedWidth(resizeByPixels, resFieldSizeCurr), getExpectedHeight(resizeByPixels, resFieldSizeCurr)));
+        softly.assertAll();
+    }
+
+    private int getExpectedHeight(int resizeByPixels, Dimension resFieldSizeOriginal) {
+        return resFieldSizeOriginal.getHeight() + resizeByPixels;
+    }
+
+    private int getExpectedWidth(int resizeByPixels, Dimension resFieldSizeOriginal) {
+        return resFieldSizeOriginal.getWidth() + resizeByPixels;
+    }
+
+    private Dimension getResFieldSize() {
+        WebElement resizableField = driver.findElement(By.id("resizable"));
+        return resizableField.getSize();
     }
 }
---
+++ src/test/java/interactions/SortableTests.java
@@ -0,0 +1,44 @@
+package interactions;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import setup.TestBase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SortableTests extends TestBase {
+    @Test
+    public void sortElements() {
+        driver.get("http://51.75.61.161:9102/sortable.php");
+        List<WebElement> elementsShuffled = driver.findElements(By.cssSelector("#sortable li"));
+
+        Collections.shuffle(elementsShuffled, new Random());
+        List<String> elementsExpectedTexts = new ArrayList<>();
+        for (WebElement element : elementsShuffled) {
+            elementsExpectedTexts.add(element.getText());
+        }
+
+        Actions actions = new Actions(driver);
+        for (int i = 0; i < elementsShuffled.size(); i++) {
+            WebElement elementByOrder = driver.findElement(By.xpath("//*[@id='sortable']//li["+(i+1)+"]"));
+            actions.dragAndDrop(elementsShuffled.get(i), elementByOrder).perform();
+        }
+
+        List<WebElement> elementsAfterSwap = driver.findElements(By.cssSelector("#sortable li"));
+        List<String> elementsActualTexts = new ArrayList<>();
+        for (WebElement element : elementsAfterSwap) {
+            elementsActualTexts.add(element.getText());
+        }
+
+        assertThat("Check that order of elements is correct", elementsExpectedTexts, equalTo(elementsActualTexts));
+    }
+}
